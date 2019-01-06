c71d63abe9cb6fcc66419ce32c742e73735c6d6b
pepe_witch <skpepe134@gmail.com>
2019-01-06 15:27:15 +0700

    add ping method

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import WooCommerceAPI from 'woocommerce-api';
 import { config } from 'dotenv';
 import * as admin from 'firebase-admin';
+import cors from 'cors';
 config();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -11,6 +12,9 @@ admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
 const app = express();
+
+app.use(cors());
+
 const WooCommerce = new WooCommerceAPI({
   url: 'https://monkeymonkeyshop.com',
   consumerKey: process.env.CONSUMER_KEY,
@@ -19,18 +23,22 @@ const WooCommerce = new WooCommerceAPI({
   wpAPI: true,
 });
 
+app.get('/ping', (req, res) => {
+  res.status(200).send({ message: 'pong' });
+});
+
 app.get('/woo/*', async (req, res) => {
   const result = await WooCommerce.getAsync(req.url.slice(5));
   return res.status(200).send(JSON.parse(result.toJSON().body));
 });
 
 app.post('/woo/*', async (req, res) => {
-  const result = await WooCommerce.postAsync(req.url.slice(5) , req.body);
+  const result = await WooCommerce.postAsync(req.url.slice(5), req.body);
   return res.status(200).send(JSON.parse(result.toJSON().body));
 });
 
 app.put('/woo/*', async (req, res) => {
-  const result = await WooCommerce.putAsync(req.url.slice(5) , req.body);
+  const result = await WooCommerce.putAsync(req.url.slice(5), req.body);
   return res.status(200).send(JSON.parse(result.toJSON().body));
 });
 
